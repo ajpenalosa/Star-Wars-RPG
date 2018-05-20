a1ea74462a3fa1fb1eef35b9c28ecbfd9f1766cc
Anthony Penalosa <aj@anthonys-mbp.attlocal.net>
2018-05-19 22:11:50 -0700

    Adding actions for choosing your character and enemy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
         {
             // Obi-Wan Kenobi
             name: "Obi-Wan Kenobi",
+            className: "obi-wan-kenobi",
             healthPoints: 120,
             attackPower: 8,
             counterAttackPower: 10, // unknown
@@ -13,6 +14,7 @@ $(document).ready(function() {
         {
             // Luke Skywalker
             name: "Luke Skywalker",
+            className: "luke-skywalker",
             healthPoints: 100,
             attackPower: 12, // unknown
             counterAttackPower: 5,
@@ -21,6 +23,7 @@ $(document).ready(function() {
         {
             // Darth Sidious
             name: "Darth Sidious",
+            className: "darth-sidious",
             healthPoints: 150,
             attackPower: 14, // unknown
             counterAttackPower: 20,
@@ -29,6 +32,7 @@ $(document).ready(function() {
         {
             // Darth Maul
             name: "Darth Maul",
+            className: "darth-maul",
             healthPoints: 180,
             attackPower: 10, // unknown
             counterAttackPower: 25,
@@ -45,13 +49,25 @@ $(document).ready(function() {
         var btnAttack = $(".btn-attack");
         var currentDefender = $(".current-defender");
         var message = $(".message");
+        
+        var characterChosen = false;
+        var enemyChosen = false;
+
+        var yourCharacter;
+        var yourEnemy;
+
+        // Your character stats
+
+        var yourHealthPoints;
+        var yourAttackPower;
+        var enemyCounterAttack;
 
         // Loop to create buttons for each character
         for ( var i = 0; i < characters.length; i++ ) {
         
             // Creating a <button> for each character
             var characterButton = $("<button>");
-            characterButton.addClass("btn-character obi-wan-kenobi");
+            characterButton.addClass("btn-character").addClass(characters[i].className).val(characters[i].className);
     
             // Creating an <img> for each character
             var characterImage = $("<img>")
@@ -66,7 +82,67 @@ $(document).ready(function() {
             // Appending characterButton to the character choices div
             characterChoices.append(characterButton);
         }
-    }
+
+        // Choose your character
+        characterChoices.on("click", ".btn-character", function() {
+
+            // Storing the button in a variable
+            var btnCharacter = $(".btn-character");
+
+            if(!characterChosen) {
+
+                // Stores the chosen character in a variable
+                yourCharacter = ($(this));
+
+                // Adding "btn-enemy" class to all buttons
+                btnCharacter.addClass("btn-enemy");
+
+                // Removes "btn-enemy" class from the chosen character
+                yourCharacter.removeClass("btn-enemy");
+
+                // Appends characters that were not chosen to the enemy choices div
+                enemyChoices.append(btnCharacter);
+                characterChoices.append(this);
+
+                characterChosen = true;
+            }
+            else {
+
+                // Attack code should happen here
+                console.log("Character has been chosen");
+            }
+        });
+
+        // Choose your enemy
+        enemyChoices.on("click", ".btn-enemy", function() {
+
+            if(!enemyChosen) {
+                yourEnemy = ($(this));
+                enemyChoices.append($(".btn-enemy"));
+                currentDefender.append(this);
+                enemyChosen = true;
+            }
+            else {
+
+                // Attack code should happen here
+                console.log("Enemy has been chosen");
+            }
+        });
+
+        // Attack button
+
+        $(".controls").on("click", ".btn-attack", function() {
+
+            if(!characterChosen) {
+                message.text("Please choose a character.");
+            }
+            else {
+
+            }
+
+        });
+
+    } // End of game function
 
     game();
 
