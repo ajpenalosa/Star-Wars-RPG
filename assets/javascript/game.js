9f93187425103e125bcd0a436ee6e8bcb6fa29b2
Anthony Penalosa <aj@anthonys-mbp.attlocal.net>
2018-05-19 20:26:09 -0700

    Changing code to create buttons for the other characters Putting the objects for each character into an array Creating a loop to create the buttons for each character

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,40 @@
 $(document).ready(function() {
-    
-    // Obi-Wan Kenobi object
-    var obiWanKenobi = {
-        healthPoints: 120,
-        attackPower: 8,
-        counterAttackPower: 10, // unknown
-        image: "./assets/images/obi-wan-kenobi.jpg"
-    };
 
-    // Luke Skywalker object
-    var lukeSkywalker = {
-        healthPoints: 100,
-        attackPower: 12, // unknown
-        counterAttackPower: 5,
-        image: "./assets/images/luke-skywalker.jpg"
-    };
-
-    // Darth Sidious object
-    var darthSidious = {
-        healthPoints: 150,
-        attackPower: 14, // unknown
-        counterAttackPower: 20,
-        image: "./assets/images/darth-sidious.jpg"
-    };
-
-    // Darth Maul object
-    var darthMaul = {
-        healthPoints: 180,
-        attackPower: 10, // unknown
-        counterAttackPower: 25,
-        image: "./assets/images/darth-maul.jpg"
-    };
+    // Array of objects for each character
+    var characters = [
+        {
+            // Obi-Wan Kenobi
+            name: "Obi-Wan Kenobi",
+            healthPoints: 120,
+            attackPower: 8,
+            counterAttackPower: 10, // unknown
+            image: "./assets/images/obi-wan-kenobi.jpg"
+        },
+        {
+            // Luke Skywalker
+            name: "Luke Skywalker",
+            healthPoints: 100,
+            attackPower: 12, // unknown
+            counterAttackPower: 5,
+            image: "./assets/images/luke-skywalker.jpg"
+        },
+        {
+            // Darth Sidious
+            name: "Darth Sidious",
+            healthPoints: 150,
+            attackPower: 14, // unknown
+            counterAttackPower: 20,
+            image: "./assets/images/darth-sidious.jpg"
+        },
+        {
+            // Darth Maul
+            name: "Darth Maul",
+            healthPoints: 180,
+            attackPower: 10, // unknown
+            counterAttackPower: 25,
+            image: "./assets/images/darth-maul.jpg"
+        }
+    ];
 
     // Creating a game function
     function game() {
@@ -41,23 +45,27 @@ $(document).ready(function() {
         var btnAttack = $(".btn-attack");
         var currentDefender = $(".current-defender");
         var message = $(".message");
+
+        // Loop to create buttons for each character
+        for ( var i = 0; i < characters.length; i++ ) {
         
-        // Creating a <div> for Obi-Wan Kenobi
-        var obiDiv = $("<div>");
-        obiDiv.addClass("character-wrapper obi-wan-kenobi");
-
-        // Creating an <img> for Obi-Wan Kenobi
-        var obiImage = $("<img>")
-        obiImage.addClass("character-image");
-        obiImage.attr("src", obiWanKenobi.image);
-
-        // Appending items to obiDiv
-        obiDiv.append("Obi-Wan Kenobi");
-        obiDiv.append(obiImage);
-        obiDiv.append(obiWanKenobi.healthPoints);
-
-        // Appending obiDiv to the character choices div
-        characterChoices.append(obiDiv);
+            // Creating a <button> for each character
+            var characterButton = $("<button>");
+            characterButton.addClass("character-wrapper obi-wan-kenobi");
+    
+            // Creating an <img> for each character
+            var characterImage = $("<img>")
+            characterImage.addClass("character-image");
+            characterImage.attr("src", characters[i].image);
+    
+            // Appending items to characterButton
+            characterButton.append(characters[i].name);
+            characterButton.append(characterImage);
+            characterButton.append(characters[i].healthPoints);
+    
+            // Appending characterButton to the character choices div
+            characterChoices.append(characterButton);
+        }
     }
 
     game();
